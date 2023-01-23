8d183cb335f277e39692c9cb8ac4c1cf05788faa
Alexander <valverin95@gmail.com>
2023-01-23 19:53:23 +0300

    test: add method for getting all elements

--- packages/select/src/utils/get.ts
+++ packages/select/src/utils/get.ts
@@ -2,3 +2,4 @@ import {TestID} from '../testID';
 import {screen} from 'solid-testing-library';
 
 export const Get = (selector: TestID) => screen.getByTestId(selector);
+export const GetAll = (selector: TestID) => screen.getAllByTestId(selector);
